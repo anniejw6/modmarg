2dbe20e12e313d9070fc348be3272c1e9425d79e
Annie J. Wang <anniejw6@gmail.com>
2016-07-24 13:18:38 -0400

    jacobian for levels works!!!

--- R/jacob_level.R
+++ R/jacob_level.R
@@ -2,37 +2,44 @@
 #'
 #' @param covar_matrix numeric matrix of covariates
 #' @param pred_values numeric vector of predicted values
-#' @param inv_link_deriv function for the derivative of the inverse link function with respect to eta
+#' @param link_deriv function for the derivative of the link function with respect to eta
 #'
 #' @return numeric vector row of jacobian
 #' @export
 #'
 #' @examples
 #'
-#' # Run model
 #' data(mtcars)
-#' mtcars$carb <- factor(mtcars$carb)
-#' mm <- glm(vs ~ carb + mpg * disp, mtcars, family = 'binomial')
+#' mtcars$gear <- factor(mtcars$gear)
+#' mm <- glm(vs ~ gear + mpg * disp, mtcars, family = 'binomial')
 #'
-#' # Create transformed covariates
-#' covar <- transform(mm$model, carb = 1)
-#' covar$`mpg:disp` <- covar$mpg * covar$disp
-#'
-#' # Generate predictions
-#' z <- predict(mm, data = covar)
-#'
-#' # Specify derivative of link function
 #' binom_family <- make.link('logit')
 #' ld_fun <- binom_family$mu.eta
 #'
-#' jacob_level(pred_values = z, covar_matrix = covar, inv_link_deriv = ld_fun)
-jacob_level <- function(pred_values, covar_matrix, inv_link_deriv){
+#' df3 <- transform(mm$data, gear = factor(3, levels = levels(mtcars$gear)))
+#' df4 <- transform(mm$data, gear = factor(4, levels = levels(mtcars$gear)))
+#' df5 <- transform(mm$data, gear = factor(5, levels = levels(mtcars$gear)))
+#'
+#' covar3 <- model.matrix(mm$formula, df3)
+#' covar4 <- model.matrix(mm$formula, df4)
+#' covar5 <- model.matrix(mm$formula, df5)
+#'
+#' # Note type != response
+#' p3 <- predict(mm, newdata = df3)
+#' p4 <- predict(mm, newdata = df4)
+#' p5 <- predict(mm, newdata = df5)
+#'
+#' jacob_level(p3, as.matrix(covar3), ld_fun)
+#' jacob_level(p4, as.matrix(covar4), ld_fun)
+#' jacob_level(p5, as.matrix(covar5), ld_fun)
+#'
+jacob_level <- function(pred_values, covar_matrix, link_deriv){
 
   stopifnot(is.numeric(pred_values), is.matrix(covar_matrix),
-            is.numeric(covar_matrix), is.function(inv_link_deriv))
+            is.numeric(covar_matrix), is.function(link_deriv))
 
   # caluclate derivative of the predicted values
-  x1 <- do.call(inv_link_deriv, list(pred_values))
+  x1 <- do.call(link_deriv, list(pred_values))
 
   as.numeric(
     crossprod(x1, covar_matrix)/nrow(covar_matrix)
